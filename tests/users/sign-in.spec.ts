2e57bd811e39d6b4f1beacbc6f8f26d5e3fba5f5
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-04-22 02:12:25 -0300

    test: add sign-in suite tests

---
+++ tests/users/sign-in.spec.ts
@@ -0,0 +1,59 @@
+import { expect, test } from '@playwright/test'
+import { Auth, signIn } from '../utils'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+  await page.click('text=Sign In')
+})
+
+test('should sign in with valid credentials', async ({ page, baseURL }) => {
+  await signIn({ page, baseURL })
+
+  const title = page.locator('h4')
+  expect(await title.innerText()).toBe('My Projects')
+})
+
+test('shound not be able to sign in with invalid email format', async ({
+  page,
+  baseURL
+}) => {
+  await page.fill('input[name="email"]', 'invalid_email')
+  await page.fill('input[name="password"]', 'invalid_password')
+  await page.click('button[type="submit"]')
+
+  expect(page.url()).toBe(`${baseURL}/login`)
+})
+
+test('should not be able to sign in with invalid password', async ({
+  page,
+  baseURL
+}) => {
+  await page.fill('input[name="email"]', Auth.EMAIL)
+  await page.fill('input[name="password"]', Math.random().toString(24))
+  await page.click('button[type="submit"]')
+
+  await page.waitForURL(`${baseURL}/login`, { waitUntil: 'networkidle' })
+
+  expect(
+    await page.locator('text=Email or Password invalid!').isVisible()
+  ).toBeTruthy()
+  expect(page.url()).toBe(`${baseURL}/login`)
+})
+
+test('should not be able to sign in with empty email and password', async ({
+  page,
+  baseURL
+}) => {
+  await page.click('button[type="submit"]')
+  expect(page.url()).toBe(`${baseURL}/login`)
+})
+
+test('should redirect to dashboard if user is already authenticated', async ({
+  page,
+  baseURL
+}) => {
+  await signIn({ page, baseURL })
+  await page.goto(`${baseURL}/login`, { waitUntil: 'networkidle' })
+
+  expect(page.url()).toBe(`${baseURL}/dashboard`)
+})
